ae75887b5873f005bfae9a078f2396c352f7c5c9
lalala <m18865203560@163.com>
2026-09-13 16:59:57 +0800

    练习：平滑切换颜色

--- main.c
+++ main.c
@@ -1,8 +1,54 @@
-#include<stdio.h>
+#include "SDL3/SDL_pixels.h"
+#include "SDL3/SDL_render.h"
+#define SDL_MAIN_USE_CALLBACKS 1
 #include<SDL3/SDL.h>
+#include<SDL3/SDL_main.h>
 
-int main(){
-    printf("Hello,world!\n");
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
 
-    return 0;
+SDL_AppResult SDL_AppInit(void **appstate,int argc,char *argv[])
+{
+    SDL_SetAppMetadata("3d","1.0","com.lalala.3d");
+
+    if(!SDL_Init(SDL_INIT_VIDEO)){
+        SDL_Log("Could't initialize SDL: %s",SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if(!SDL_CreateWindowAndRenderer("examples/renderer/clear",640,480,
+                                    SDL_WINDOW_RESIZABLE,&window,&renderer)){
+        SDL_Log("same");
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(renderer,640,480,SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate,SDL_Event *event)
+{
+    if(event->type == SDL_EVENT_QUIT){
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    const double now = ((double)SDL_GetTicks()) / 1000.0;
+    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
+    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+    SDL_SetRenderDrawColorFloat(renderer,red,green,blue,SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate,SDL_AppResult result)
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 }
